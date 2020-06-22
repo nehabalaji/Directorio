773e0786e44e4bc1726655f79dbf08b2a845ff2a
nehabalaji <nehabalaji10@gmail.com>
2020-06-22 20:14:35 +0530

    initial commit

---
+++ app/src/main/java/com/example/contactsapp/Contacts.java
@@ -0,0 +1,106 @@
+package com.example.contactsapp;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "Contacts")
+public class Contacts {
+
+    @NonNull
+    @PrimaryKey
+    @ColumnInfo(name = "Name")
+    private String Name;
+
+    @ColumnInfo(name = "Phone Number")
+    private String Number;
+
+    @ColumnInfo(name = "email")
+    private String email;
+
+    @ColumnInfo(name = "Age")
+    private String Age;
+
+    @ColumnInfo(name = "Gender")
+    private String Gender;
+
+    @ColumnInfo(name = "City")
+    private String City;
+
+    @ColumnInfo(name = "College")
+    private String College;
+
+    public Contacts(@NonNull String name, String number, String email, String age, String gender, String city, String college) {
+        this.Name = name;
+        this.Number = number;
+        this.email = email;
+        this.Age = age;
+        this.Gender = gender;
+        this.City = city;
+        this.College = college;
+    }
+
+    public Contacts(){
+
+    }
+
+    @NonNull
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(@NonNull String name) {
+        Name = name;
+    }
+
+    public String getNumber() {
+        return Number;
+    }
+
+    public void setNumber(String number) {
+        Number = number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAge() {
+        return Age;
+    }
+
+    public void setAge(String age) {
+        Age = age;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String gender) {
+        Gender = gender;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String city) {
+        City = city;
+    }
+
+    public String getCollege() {
+        return College;
+    }
+
+    public void setCollege(String college) {
+        College = college;
+    }
+}
+
+
